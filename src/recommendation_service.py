7e70f8c2c9d90a8eb55eb3fb24f6afb4d3267959
drashti927 <jvlfc@iup.edu>
2026-05-01 19:49:46 -0400

    Update recommendation_service.py

--- src/recommendation_service.py
+++ src/recommendation_service.py
@@ -1,8 +1,9 @@
-from flask_cors import CORS
 from flask import Flask, request, jsonify
 import requests
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route("/recommend")
 def recommend():
